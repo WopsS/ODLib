5b30557088a8d61162788a1d3d137671f70c7212
Octavian Dima <hello@wopss.ro>
2017-07-31 10:13:16 +0300

    Update "version.hpp"

--- Source/odlib/version.hpp
+++ Source/odlib/version.hpp
@@ -7,16 +7,16 @@ namespace odlib
         /// <summary>
         /// Get ODLib's version as an integer.  
         /// </summary>  
-        const uint32_t id = 100;
+        const uint32_t id = 800;
 
         /// <summary>
         /// Get ODLib's version as a string.  
         /// </summary>  
-        const std::string string = "0.1.0";
+        const std::string string = "0.8.0";
 
         /// <summary>
         /// Get ODLib's version as a wstring.  
         /// </summary>  
-        const std::wstring wstring = L"0.1.0";
+        const std::wstring wstring = L"0.8.0";
     }
 }
