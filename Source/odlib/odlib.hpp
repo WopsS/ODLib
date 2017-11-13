3bbd2ac426770414bd2f3d7a58b2b0a4530a2e1c
Octavian Dima <hello@wopss.ro>
2017-11-13 18:21:13 +0100

    Build fix

--- Source/odlib/odlib.hpp
+++ Source/odlib/odlib.hpp
@@ -10,13 +10,13 @@
 #include <iostream>
 #include <iterator>
 #include <locale>
-#include <map>
 #include <memory>
 #include <mutex>
 #include <queue>
 #include <sstream>
 #include <string>
 #include <thread>
+#include <unordered_map>
 
 #include <odlib/version.hpp>
 #include <odlib/singleton/define.hpp>
