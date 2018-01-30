0c44a7dd6e330ed08498aeed92b4329a87a57dcc
Octavian Dima <hello@wopss.ro>
2018-01-30 05:42:41 +0100

    Fix some mistakes in "utils::number"

--- Source/odlib/utils/number.hpp
+++ Source/odlib/utils/number.hpp
@@ -20,7 +20,7 @@ namespace odlib
             template<typename T>
             const bool is_long_double(const std::basic_string<T>& text)
             {
-                double number;
+                long double number;
 
                 std::basic_istringstream<T> stream(text);
                 stream >> std::noskipws >> number;
@@ -61,12 +61,20 @@ namespace odlib
             template<typename T, typename Elem>
             const bool is_valid(const std::basic_string<Elem>& text)
             {
-                T value;
+                // The stream will insert just one character for char types, prevent this by trying to convert it to short.
+                if constexpr (std::is_same_v<T, char> || std::is_same_v<T, int8_t> || std::is_same_v<T, unsigned char> || std::is_same_v<T, uint8_t>)
+                {
+                    return is_valid<int16_t>(text);
+                }
+                else
+                {
+                    T value;
 
-                std::basic_istringstream<Elem> stream(text);
-                stream >> value;
+                    std::basic_istringstream<Elem> stream(text);
+                    stream >> value;
 
-                return stream.eof() && (stream.fail() == false);
+                    return stream.eof() && (stream.fail() == false);
+                }
             }
         }
     }
